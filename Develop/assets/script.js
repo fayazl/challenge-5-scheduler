6febfbf5125e08655e3cd6683a9dc7136de1b2f0
Fayaz <flalani100@gmail.com>
2021-11-06 11:05:23 -0400

    color change created

--- Develop/assets/script.js
+++ Develop/assets/script.js
@@ -1,12 +1,34 @@
 const today = moment().format('LL')
-const todayTime = moment.format()
+var timeNow = moment().hour()
+var saveBtn = $("saveBtn")
+
+
 
 $( "#currentDay").append("Today's date is: " + today)
 
-//Dynamic color change based on time
 
-function textareaColor(){
+//dynamic color change
+function textAreaColor(){
+    $( ".time-block").each(function(){
+        
+        var currentHour = parseInt($(this).attr("id"));
+        console.log(currentHour);
 
-}
+        if (currentHour < timeNow){
+            $(this).addClass("past")
+        }
+
+        else if (currentHour === timeNow){
+            $(this).addClass("present")
+        }
+
+        else {
+            $(this).addClass("future")
+        }
+
+});     
+
+};
 
 
+textAreaColor()
