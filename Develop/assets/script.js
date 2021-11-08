f29f5daf6f09650c7779a9e6d35459734e6fc322
Fayaz <flalani100@gmail.com>
2021-11-07 22:26:58 -0500

    test commit for pages

---
+++ Develop/assets/script.js
@@ -0,0 +1,67 @@
+const today = moment().format('LL')
+var timeNow = moment().hour()
+var saveBtn = $("saveBtn")
+
+
+// Today's date on the top of the page
+$( "#currentDay").append("Today's date is: " + today)
+
+
+//dynamic color change
+function textAreaColor(){
+    $( ".time-block").each(function(){
+        
+        var currentHour = parseInt($(this).attr("id"));
+        console.log(currentHour);
+
+        if (currentHour < timeNow){
+            $(this).addClass("past")
+        }
+
+        else if (currentHour === timeNow){
+            $(this).addClass("present")
+        }
+
+        else {
+            $(this).addClass("future")
+        }
+
+    });     
+
+};
+
+//save data function
+function saveData(){
+    $(".saveBtn").on("click", function(){
+
+        var time = $(this).siblings(".hour").text()
+        var text = $(this).siblings(".description").val()
+
+        console.log(time);
+        console.log(text)
+
+        localStorage.setItem(time, text);
+    });
+};
+
+//maintain data on refresh
+function maintainData(){
+    $(".hour").each(function(){
+
+        let savedHour = $(this).text();
+        //  console.log(savedHour);
+
+        let storedText = localStorage.getItem(savedHour);
+        //  console.log(storedText);
+
+        if (storedText !== null){
+            $(this).siblings(".description").val(storedText);
+        }
+    });   
+
+ };
+
+//run function
+textAreaColor()
+saveData()
+maintainData()
