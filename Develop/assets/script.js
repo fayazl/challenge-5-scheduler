47976c656cdc942a29ad082b7fd13fab735d2cbf
Fayaz <flalani100@gmail.com>
2021-11-07 21:21:48 -0500

    added button saving

--- Develop/assets/script.js
+++ Develop/assets/script.js
@@ -3,7 +3,7 @@ var timeNow = moment().hour()
 var saveBtn = $("saveBtn")
 
 
-
+// Today's date on the top of the page
 $( "#currentDay").append("Today's date is: " + today)
 
 
@@ -26,9 +26,37 @@ function textAreaColor(){
             $(this).addClass("future")
         }
 
-});     
+    });     
 
 };
 
+//save data function
+function saveData(){
+    $(".saveBtn").on("click", function(){
 
+        var time = $(this).siblings(".hour").text()
+        var text = $(this).siblings(".description").val()
+
+        console.log(time);
+        console.log(text)
+
+        localStorage.setItem(time, text);
+    });
+};
+
+// //maintain data on refresh
+// function maintainData(){
+//     $( ".description").each(function(){
+//         var currentHour = $(this).text();
+//         var storedText = localStorge.getItem(currentHour);
+
+//         if (storedText !== null){
+//             $(this).siblings(".description").val(storedText);
+//         }
+//     })
+// };
+
+//run function
 textAreaColor()
+saveData()
+
